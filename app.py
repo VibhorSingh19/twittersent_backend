606eceaeb90417ed9d6a0e0ad5855755cccc38fe
Vibhor Singh <vibhor.formal@gmail.com>
2021-06-01 17:17:23 +0530

    Updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from tweepy import OAuthHandler
 from textblob import TextBlob
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
 api = Api(app)
 
 class TwitterClient(Resource):
